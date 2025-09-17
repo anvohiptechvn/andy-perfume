aa9718750329e108464e6431862e2853577f64ec
phucle1010 <lethephuc2002@gmail.com>
2025-09-17 23:16:58 +0700

    feat: information secure policy page UI

--- src/app/chinh-sach-mua-hang/components/index.tsx
+++ src/app/chinh-sach-mua-hang/components/index.tsx
@@ -7,61 +7,61 @@ const PurchasePolicyPage = () => {
 		<PageWrapper>
 			<h1 className="mt-2 text-lg font-medium text-[#323c3f] md:text-xl">Chính sách mua hàng</h1>
 
-			<p className="tex-md text-[#42495b] md:text-base">
+			<p className="tex-sm text-[#42495b] md:text-base">
 				Parfumerie luôn mong muốn mang đến cho Quý Khách Hàng những trải nghiệm dịch vụ mua sắm tốt nhất. Chúng tôi phục vụ nhu cầu mua hàng trên toàn quốc với
 				chính sách mua hàng cụ thể như sau:
 			</p>
 			<h3 className="text-sm font-bold text-[#42495b] md:text-base">1. Sản phẩm nước hoa chiết:</h3>
 			<h3 className="text-sm font-bold text-[#42495b] md:text-base">1.1. Đặc điểm của nước hoa chiết.</h3>
 
-			<p className="tex-md text-[#42495b] md:text-base">
+			<p className="tex-sm text-[#42495b] md:text-base">
 				Nước hoa chiết được hiểu đơn giản là nước hoa rút ra từ những chai nước hoa fullsize chính hãng có dung tích 100ml - 200ml. Chai chiết thường được làm
 				bằng thuỷ tinh có vòi xịt, tuỳ nhu cầu của Quý Khách Hàng mà Parfumerie sẽ chiết ra lượng nước hoa có dung tích từ 10ml - 20ml - 30ml... để Quý Khách
 				Hàng trải nghiệm sản phẩm.
 			</p>
-			<p className="tex-md text-[#42495b] md:text-base">- Chai chiết có ưu điểm là nhỏ gọn, tiện mang theo đi làm, đi chơi hay đi du lịch.</p>
-			<p className="tex-md text-[#42495b] md:text-base">- Nước hoa chiết có chất lượng y như nước hoa fullsize chính hãng.</p>
-			<p className="tex-md text-[#42495b] md:text-base">
+			<p className="tex-sm text-[#42495b] md:text-base">- Chai chiết có ưu điểm là nhỏ gọn, tiện mang theo đi làm, đi chơi hay đi du lịch.</p>
+			<p className="tex-sm text-[#42495b] md:text-base">- Nước hoa chiết có chất lượng y như nước hoa fullsize chính hãng.</p>
+			<p className="tex-sm text-[#42495b] md:text-base">
 				*** Đối với Quý Khách Hàng không có mục tiêu chi tiêu số tiền lớn cho nước hoa và/hoặc muốn trải nghiệm được nhiều mùi hương khác nhau thì NƯỚC HOA
 				CHIẾT sẽ là một lựa chọn khá hợp lý.
 			</p>
 
 			<h3 className="text-sm font-bold text-[#42495b] md:text-base">1.2. Chính sách mua nước hoa chiết tại Parfumerie.</h3>
-			<p className="tex-md text-[#42495b] md:text-base">
+			<p className="tex-sm text-[#42495b] md:text-base">
 				- Quý Khách Hàng được yêu cầu đặt cọc <strong>Phí vận chuyển và Bảo hiểm hàng hoá</strong> theo Chính sách được quy định cụ thể vào từng thời điểm của
 				Cửa hàng. Trường hợp Quý Khách Hàng không nhận hàng thì số tiền đặt cọc sẽ được dùng để thanh toán toàn bộ Phí vận chuyển và Bảo hiểm hàng hoá của đơn
 				hàng.
 			</p>
 
-			<p className="tex-md text-[#42495b] md:text-base">- Quý Khách Hàng được tự do thử mùi, test sản phẩm tại Hệ thống cửa hàng của Parfumerie.</p>
-			<p className="tex-md text-[#42495b] md:text-base">
+			<p className="tex-sm text-[#42495b] md:text-base">- Quý Khách Hàng được tự do thử mùi, test sản phẩm tại Hệ thống cửa hàng của Parfumerie.</p>
+			<p className="tex-sm text-[#42495b] md:text-base">
 				- Quý Khách Hàng được áp dụng Chính sách thanh toán/ vận chuyển/ đổi trả quy định trên website parfumerie.vn.
 			</p>
-			<p className="tex-md text-[#42495b] md:text-base">- Hotline chăm sóc khách hàng 24/07: 0888070308.</p>
+			<p className="tex-sm text-[#42495b] md:text-base">- Hotline chăm sóc khách hàng 24/07: 0888070308.</p>
 
 			<h3 className="text-sm font-bold text-[#42495b] md:text-base">2. Sản phẩm nước hoa chai lớn chưa mở hộp (Fullbox)</h3>
 			<h3 className="text-sm font-bold text-[#42495b] md:text-base">2.1. Đặc điểm của sản phẩm nước hoa Fullbox.</h3>
-			<p className="tex-md text-[#42495b] md:text-base">
+			<p className="tex-sm text-[#42495b] md:text-base">
 				- Parfumerie luôn tìm kiếm những nguồn cung cấp nước hoa chất lượng và đảm bảo là nước hoa chính hãng đến tay khách hàng với chi phí tốt nhất.
 			</p>
-			<p className="tex-md text-[#42495b] md:text-base">- Parfumerie 100% nói KHÔNG với nước hoa là hàng giả/ hàng nhái (fake).</p>
-			<p className="tex-md text-[#42495b] md:text-base">
+			<p className="tex-sm text-[#42495b] md:text-base">- Parfumerie 100% nói KHÔNG với nước hoa là hàng giả/ hàng nhái (fake).</p>
+			<p className="tex-sm text-[#42495b] md:text-base">
 				- Parfumerie 100% nói KHÔNG với "bán hàng bất chấp" và "cạnh tranh không lành mạnh" với các cửa hàng/ đơn vị kinh doanh sản phẩm tương tự trên thị
 				trường.
 			</p>
 
 			<h3 className="text-sm font-bold text-[#42495b] md:text-base">2.2. Chính sách mua nước hoa Fullbox tại Parfumerie.</h3>
-			<p className="tex-md text-[#42495b] md:text-base">
+			<p className="tex-sm text-[#42495b] md:text-base">
 				- Quý Khách Hàng được yêu cầu đặt cọc <strong>Phí vận chuyển và Bảo hiểm hàng hoá</strong> đối với các sản phẩm nước hoa Fullbox có giá trị từ
 				1.000.000đ trở lên hoặc theo Chính sách được quy định cụ thể vào từng thời điểm của Cửa hàng. Trường hợp Quý Khách Hàng không nhận hàng thì số tiền đặt
 				cọc sẽ được dùng để thanh toán toàn bộ Phí vận chuyển và Bảo hiểm hàng hoá của đơn hàng.
 			</p>
-			<p className="tex-md text-[#42495b] md:text-base">- Quý Khách Hàng được tự do thử mùi, test sản phẩm tại Hệ thống cửa hàng của Parfumerie.</p>
-			<p className="tex-md text-[#42495b] md:text-base">
+			<p className="tex-sm text-[#42495b] md:text-base">- Quý Khách Hàng được tự do thử mùi, test sản phẩm tại Hệ thống cửa hàng của Parfumerie.</p>
+			<p className="tex-sm text-[#42495b] md:text-base">
 				- Quý Khách Hàng được áp dụng Chính sách thanh toán/ vận chuyển/ đổi trả quy định trên website parfumerie.vn.
 			</p>
-			<p className="tex-md text-[#42495b] md:text-base">- Hotline chăm sóc khách hàng 24/07: 0888070308.</p>
-			<p className="tex-md text-[#42495b] md:text-base">Trân trọng cám ơn.</p>
+			<p className="tex-sm text-[#42495b] md:text-base">- Hotline chăm sóc khách hàng 24/07: 0888070308.</p>
+			<p className="tex-sm text-[#42495b] md:text-base">Trân trọng cám ơn.</p>
 		</PageWrapper>
 	);
 };
