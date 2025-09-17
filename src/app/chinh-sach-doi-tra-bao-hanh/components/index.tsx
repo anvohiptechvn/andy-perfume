c9c2134d777651ef5af26ac2d5db544c6b651edc
phucle1010 <lethephuc2002@gmail.com>
2025-09-17 23:35:28 +0700

    feat: return and assurance policy page UI

--- src/app/chinh-sach-doi-tra-bao-hanh/components/index.tsx
+++ src/app/chinh-sach-doi-tra-bao-hanh/components/index.tsx
@@ -5,7 +5,62 @@ import PageWrapper from '@/components/feature/page/page-wrapper';
 const ReturnAssurancePolicyPage = () => {
 	return (
 		<PageWrapper>
-			<div></div>
+			<h1 className="text-lg font-medium text-[#323c3f] md:text-xl">Chính sách đổi trả - bảo hành</h1>
+			<p className="tex-sm text-[#42495b] md:text-base">
+				Với mong muốn mang lại sự tin tưởng & hài lòng cho Quý khách trải nghiệm mua hàng, Parfumerievn cam kết tất cả sản phẩm Parfumerievn bán ra đảm bảo là
+				hàng chính hãng 100%, chúng tôi không bán hàng giả, hàng nhái, hàng không đảm bảo chất lượng... Chúng tôi phục vụ nhu cầu mua hàng trên toàn quốc với
+				chính sách cụ thể như sau:
+			</p>
+			<p className="tex-sm text-[#42495b] md:text-base">
+				1. Quý khách vui lòng kiểm tra kỹ <strong>thông tin và dung tích</strong> sản phẩm trước khi mua hàng.
+			</p>
+			<p className="tex-sm text-[#42495b] md:text-base">
+				2. Thời hạn đổi sản phẩm <strong>trong vòng 03 ngày</strong> kể từ khi Quý Khách Hàng nhận sản phẩm của Parfumerievn tại Cửa hàng hoặc từ Đơn vị giao
+				hàng.
+			</p>
+			<p className="tex-sm text-[#42495b] md:text-base">
+				3. Chỉ áp dụng đổi sản phẩm <strong>nước hoa Fullbox</strong> bị lỗi kỹ thuật, không áp dụng cho Nước hoa chiết.
+			</p>
+			<p className="tex-sm text-[#42495b] md:text-base">
+				4. Việc đổi hàng chỉ áp dụng với những <strong>sản phẩm bị lỗi kỹ thuật</strong> do nhà sản xuất.
+			</p>
+			<ol className="list-inside list-disc space-y-1 text-sm text-[#42495b] md:text-base">
+				<li>
+					Quý Khách Hàng vui lòng thông báo ngay khi kiểm tra hàng lúc nhận và cần <strong>lập biên bản</strong> xác nhận giữa người mua và nhân viên giao hàng
+					trong trường hợp sản phẩm nước hoa bị đổ, vỡ, rò rỉ hoặc các lỗi vật lý khác bên ngoài.
+				</li>
+				<li>
+					Quý Khách Hàng vui lòng cung cấp <strong>hình ảnh & video thấy rõ lỗi kỹ thuật</strong> của sản phẩm gửi cho Parfumerievn để xác minh từ lúc bắt đầu
+					khui seal (lớp nilong bên ngoài) và hộp sản phẩm có đầy đủ TEM của Parfumerievn còn nguyên vẹn.
+				</li>
+				<li>
+					Các sản phẩm được Parfumerievn xác minh bị lỗi kỹ thuật do nhà sản xuất sẽ được <strong>đổi sản phẩm mới cùng loại</strong> cho Quý Khách Hàng trong
+					thời gian sớm nhất.
+				</li>
+			</ol>
+			<p className="tex-sm text-[#42495b] md:text-base">
+				5. Sản phẩm mua rồi, Quý khách hàng vui lòng <strong>không trả hàng nếu không phải lỗi của sản phẩm.</strong>
+			</p>
+			<p className="tex-sm text-[#42495b] md:text-base">
+				6. Bảo hành: Nước hoa là sản phẩm đặc thù nên <strong>không áp dụng</strong> chính sách bảo hành.
+			</p>
+			<p className="tex-sm text-[#42495b] md:text-base">7. Các trường hợp từ chối đổi trả - bảo hành:</p>
+			<ol className="list-inside list-disc space-y-1 text-sm text-[#42495b] md:text-base">
+				<li>Quá thời hạn quy định.</li>
+				<li>
+					Không có video từ lúc bắt đầu khui seal (lớp nilong bên ngoài hộp) của sản phẩm, TEM của Parfumerievn không còn nguyên vẹn trong quá trình quay video.
+				</li>
+				<li>Video khui sản phẩm được cắt ghép - chỉnh sửa.</li>
+				<li>Không phải lỗi kỹ thuật của sản phẩm.</li>
+				<li>Không xác minh được do Parfumerievn cung cấp.</li>
+				<li>Không có TEM của Parfumerievn hoặc TEM Parfumerievn bị rách/vỡ không còn nguyên vẹn.</li>
+				<li>Sản phẩm không còn nguyên vẹn, bị biến dạng hoặc hư hỏng nặng.</li>
+				<li>Các sản phẩm giảm giá từ 30% trở lên.</li>
+			</ol>
+			<p className="tex-sm text-[#42495b] md:text-base">8. Chính sách hoàn tiền:</p>
+			<p className="tex-sm text-[#42495b] md:text-base">Chúng tôi không áp dụng chính sách hoàn tiền. Các sản phẩm đã mua sẽ không được hoàn tiền trong mọi</p>
+			trường hợp. Sản phẩm chỉ được áp dụng đổi trả theo chính sách đổi trả đã nêu. Xin quý khách hàng vui lòng đọc kỹ thông tin trước khi mua hàng.
+			<p className="tex-sm text-[#42495b] md:text-base">Trân trọng cảm ơn!</p>
 		</PageWrapper>
 	);
 };
