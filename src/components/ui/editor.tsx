dc2c3beb426f8cd52f42c29e6b6457dd5dc3c2d3
phucle1010 <lethephuc2002@gmail.com>
2025-09-25 09:58:10 +0700

    feature: add copy button for raw html

--- src/components/ui/editor.tsx
+++ src/components/ui/editor.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import Underline from "@tiptap/extension-underline";
@@ -10,6 +10,8 @@ import Blockquote from "@tiptap/extension-blockquote";
 import Link from "@tiptap/extension-link";
 import {
   Bold,
+  Copy,
+  CopyCheck,
   ImageUp,
   Italic,
   LinkIcon,
@@ -24,6 +26,7 @@ import { cn } from "@/utils/style";
 
 export default function Editor({ initial = "" }: { initial?: string }) {
   const [content, setContent] = useState<string>("");
+  const [copied, setCopied] = useState<boolean>(false);
 
   const editor = useEditor({
     immediatelyRender: false,
@@ -41,7 +44,19 @@ export default function Editor({ initial = "" }: { initial?: string }) {
     onUpdate: ({ editor }) => setContent(editor.getHTML()),
   });
 
-  const addImage = (e: React.ChangeEvent<HTMLInputElement>) => {
+  useEffect(() => {
+    if (!copied) return;
+
+    const timer = setTimeout(() => {
+      setCopied(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [copied]);
+
+  const onAddImage = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
     if (!file || !editor) return;
     const reader = new FileReader();
@@ -55,10 +70,17 @@ export default function Editor({ initial = "" }: { initial?: string }) {
     reader.readAsDataURL(file);
   };
 
-  const setLink = () => {
+  const onUpdateLink = () => {
     const url = prompt("Enter a URL");
     if (url) {
       editor?.chain().focus().setLink({ href: url }).run();
+    }
+  };
+
+  const onCopy = () => {
+    if (navigator && navigator.clipboard) {
+      navigator.clipboard.writeText(content);
+      setCopied(true);
     }
   };
 
@@ -120,7 +142,7 @@ export default function Editor({ initial = "" }: { initial?: string }) {
             <Italic size={16} />
           </button>
           <button
-            onClick={setLink}
+            onClick={onUpdateLink}
             className={cn(
               "p-2 hover:bg-gray-100 transition-all duration-200 cursor-pointer",
               editor.isActive("link") ? "font-bold text-blue-600" : ""
@@ -150,7 +172,7 @@ export default function Editor({ initial = "" }: { initial?: string }) {
           {/* Image upload */}
           <label className="p-2 hover:bg-gray-100 transition-all duration-200 cursor-pointer">
             <ImageUp size={16} />
-            <input type="file" accept="image/*" onChange={addImage} hidden />
+            <input type="file" accept="image/*" onChange={onAddImage} hidden />
           </label>
 
           {/* Blockquote */}
@@ -190,9 +212,21 @@ export default function Editor({ initial = "" }: { initial?: string }) {
         <EditorContent editor={editor} className="h-full" />
       </div>
 
-      <h3 className="mt-10 font-bold">Raw HTML output:</h3>
-      <div className="border p-2">
-        <p className="break-words">{content}</p>
+      <div className="flex flex-col gap-4 mt-10">
+        <div className="flex items-center justify-between">
+          <h3 className="font-bold">Raw HTML output:</h3>
+          <button
+            className="px-3 py-1 border rounded hover:bg-gray-100 text-sm cursor-pointer flex items-center gap-2"
+            onClick={onCopy}
+          >
+            {copied ? <CopyCheck size={16} /> : <Copy size={16} />}
+            <span>{copied ? "Copied" : "Copy"}</span>
+          </button>
+        </div>
+
+        <div className="border p-2 min-h-40">
+          <p className="break-words">{content}</p>
+        </div>
       </div>
     </div>
   );
